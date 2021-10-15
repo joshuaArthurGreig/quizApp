6651c75b115748aca64ebdac5d88c55ecc93c1c3
joshuaArthurGreig <jagreig15@gmail.com>
2021-10-15 10:03:28 -0700

    super almost ish

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.quizapp
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
     lateinit var a1 : Button
@@ -22,9 +27,27 @@ class MainActivity : AppCompatActivity() {
         wireWidgets()
 
         val inputStream = resources.openRawResource(R.raw.questions)
-        val jsonText = inputStream.bufferedReader(
+        val jsonText = inputStream.bufferedReader().use {
+            it.readText()
+        }
+        Log.d(TAG, "onCreate : $jsonText")
+
+        val gson = Gson()
+        val type = object : TypeToken<List<Question>>() {}.type
+        val questions = gson.fromJson<List<Question>>(jsonText, type)
+        Log.d(TAG, "onCreate : \n${questions.toString()}")
+
+        quiz = Quiz(questions)
+
+        a1.setOnClickListener {
+            quiz.score(0)
+            scoreText.text = "Score :" + quiz.giveScore()
+            if(quiz.noMoreQuestions()) {
+
+            }
+        }
     }
-    }
+
 
     fun wireWidgets() {
         a1 = findViewById(R.id.button_main_a1)
@@ -33,6 +56,16 @@ class MainActivity : AppCompatActivity() {
         a4 = findViewById(R.id.button_main_a4)
         qText = findViewById(R.id.textView_main_questionText)
         scoreText = findViewById(R.id.textView_main_scoreTrack)
+    }
+
+    fun gameOver() {
+        a1.visibility = View.GONE
+        a2.visibility = View.GONE
+        a3.visibility = View.GONE
+        a4.visibility = View.GONE
+        if(quiz.checkResults()) {
+            qText.text =
+        }
     }
 
 }
